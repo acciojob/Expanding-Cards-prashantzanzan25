3624967a9efbfca7b0855026d90881d303b6b263
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-10-28 16:04:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel, index) => {
+    panel.id = `panel-${index + 1}`;
+    panel.addEventListener('click', () => {
+        panels.forEach((p) => p.classList.remove('active'));
+        panel.classList.add('active');
+    });
+});
+
